fix s shapemode calculation + stereotpy + plotting changes

src/features/evolve.py
@@ -10,8 +10,16 @@ from pathlib import Path
 from sklearn.decomposition import PCA
 from src.models.utils import apply_sample_points, get_iae_reconstruction_3d_grid
 from src.features.reconstruction import save_pcloud
+import warnings
 
-# from pointcloudutils.networks import LatentLocalDecoder
+try:
+    from pointcloudutils.networks import LatentLocalDecoder
+except ImportError:
+    warnings.warn("local_settings failed to import", ImportWarning)
+    class LatentLocalDecoder():
+        def __init__():
+            pass
+
 from src.models.predict_model import model_pass
 import random
 from sklearn.decomposition import PCA
@@ -159,8 +167,7 @@ def model_pass_reconstruct(
     z = torch.tensor(z).float().to(device)
     fraction = round(fraction, 1)
 
-    # if key == "pcloud" and not isinstance(model.decoder[key], LatentLocalDecoder):
-    if key == "pcloud":
+    if key == "pcloud" and not isinstance(model.decoder[key], LatentLocalDecoder):
         init_x = init_x[:, :, :3]
         final_x = final_x[:, :, :3]
     if hasattr(model, "network"):

src/features/outlier_compactness.py
@@ -23,9 +23,10 @@ def get_embedding_metrics(all_ret, num_PCs=None, max_embed_dim=192, method="mle"
         this_mo = all_ret.loc[all_ret["model"] == model].reset_index(drop=True)
         val = compactness(this_mo, num_PCs, max_embed_dim, method)
         percent_same = outlier_detection(this_mo)
-        ret_dict_compactness["model"].append(model)
-        ret_dict_compactness["compactness"].append(val)
-        ret_dict_compactness["percent_same"].append(percent_same)
+        for i in range(len(val)):
+            ret_dict_compactness["model"].append(model)
+            ret_dict_compactness["compactness"].append(val[i])
+            ret_dict_compactness["percent_same"].append(percent_same)
     ret_dict_compactness = pd.DataFrame(ret_dict_compactness)
     return ret_dict_compactness
 
@@ -79,7 +80,7 @@ def compute_MLE_intrinsic_dimensionality(feats, k_list=None):
     for k in k_list:
         est_dim = _intrinsic_dim_sample_wise(feats, k, dist)
         all_estimates.append(est_dim)
-    return np.avg(all_estimates), np.std(all_estimates)
+    return [np.mean(i) for i in all_estimates], np.mean(all_estimates)
 
 
 def compactness(this_mo, num_PCs, max_embed_dim, method):
@@ -91,8 +92,9 @@ def compactness(this_mo, num_PCs, max_embed_dim, method):
     this_feats = this_mo[cols].iloc[:, :max_embed_dim].dropna(axis=1).values
     if method == "pca":
         _, _, val = compute_PCA_expl_var(this_feats, num_PCs)
+        val = [val]
     elif method == "mle":
-        val, std_val = compute_MLE_intrinsic_dimensionality(this_feats)
+        val, mean_val = compute_MLE_intrinsic_dimensionality(this_feats)
     else:
         raise NotImplementedError
     return val
@@ -109,6 +111,7 @@ def outlier_detection(this_mo, outlier_label=0):
     the total population
     """
     if "flag_comment" in this_mo.columns:
+        print("Outlier column is flag comment")
         this_mo1 = this_mo.loc[
             this_mo["flag_comment"].isin(
                 ["cell appears dead or dying", "no EGFP fluorescence"]
@@ -123,18 +126,21 @@ def outlier_detection(this_mo, outlier_label=0):
         this_mo2["outlier"] = "No"
         this_mo = pd.concat([this_mo1, this_mo2], axis=0).reset_index(drop=True)
     elif "Anomaly" in this_mo.columns:
+        print("Outlier column is Anamoly")
         this_mo1 = this_mo.loc[~this_mo["Anomaly"].isin(["none"])]
         this_mo1["outlier"] = "Yes"
         this_mo2 = this_mo.loc[this_mo["Anomaly"].isin(["none"])]
         this_mo2["outlier"] = "No"
         this_mo = pd.concat([this_mo1, this_mo2], axis=0).reset_index(drop=True)
     elif "cell_stage" in this_mo.columns:
+        print("Outlier column is cell stage")
         this_mo1 = this_mo.loc[~this_mo["cell_stage"].isin(["M0"])]
         this_mo1["outlier"] = "Yes"
         this_mo2 = this_mo.loc[this_mo["cell_stage"].isin(["M0"])]
         this_mo2["outlier"] = "No"
         this_mo = pd.concat([this_mo1, this_mo2], axis=0).reset_index(drop=True)
     elif "outlier" not in this_mo.columns:
+        print("Outlier column is outlier")
         return 0
 
     if this_mo["outlier"].isna().any():

src/features/plot.py
@@ -3,9 +3,12 @@ import plotly.graph_objects as go
 import plotly.offline as pyo
 import seaborn as sns
 import os
+import matplotlib.pyplot as plt
 from pathlib import Path
 import plotly.express as px
+import numpy as np
 import random
+from .utils import normalize_intensities_and_get_colormap
 
 METRIC_DICT = {
     "recon": {"metric": ["loss"], "min": [True]},
@@ -14,7 +17,7 @@ METRIC_DICT = {
     "emissions": {"metric": ["emissions", "inference_time"], "min": [True, True]},
     "evolve": {"metric": ["energy", "closest_embedding_distance"], "min": [True, True]},
     "equiv": {"metric": ["value3"], "min": [True]},
-    "compactness": {"metric": ["compactness", "percent_same"], "min": [False, False]},
+    "compactness": {"metric": ["compactness", "percent_same"], "min": [True, False]},
 }
 
 
@@ -61,6 +64,8 @@ def collect_outputs(path, norm, model_order=None):
         "classical_image",
         "so2_image",
         "2048_ed_mae",
+        "2048_ed_dgcnn_more",
+        "2048_int_ed_vndgcnn_more",
     ]
     run_names = [
         "DGCNN",
@@ -72,6 +77,8 @@ def collect_outputs(path, norm, model_order=None):
         "ImageAE",
         "SO2 ImageAE",
         "Point MAE",
+        "DGCNN more",
+        "VN-DGCNN int more",
     ]
     rep_dict = {i: j for i, j in zip(run_names_orig, run_names)}
 
@@ -88,25 +95,33 @@ def collect_outputs(path, norm, model_order=None):
     ]:
         this_df = pd.read_csv(path + f"{metric}.csv")
         this_df["model"] = this_df["model"].replace(rep_dict)
+
         if "split" in this_df.columns:
             this_metrics = METRIC_DICT[metric]["metric"]
             # tmp_agg = this_df.loc[this_df["split"] == "test"].reset_index()
             tmp_agg = this_df
-            print(tmp_agg.shape)
-            tmp_agg = pd.melt(tmp_agg, id_vars=["model"], value_vars=this_metrics)
+            tmp_agg = pd.melt(
+                tmp_agg[["model", "split"] + this_metrics],
+                id_vars=["model"],
+                value_vars=this_metrics,
+            )
             df_non_agg.append(tmp_agg)
             this_df = (
                 this_df.loc[this_df["split"] == "test"]
-                .groupby("model")
+                .groupby(["model", "split"])
                 .mean()
                 .reset_index()
             )
         else:
             this_metrics = METRIC_DICT[metric]["metric"]
-            tmp_agg = pd.melt(this_df, id_vars=["model"], value_vars=this_metrics)
+            tmp_agg = pd.melt(
+                this_df[["model"] + this_metrics],
+                id_vars=["model"],
+                value_vars=this_metrics,
+            )
             df_non_agg.append(tmp_agg.reset_index())
+            this_df = this_df.groupby(["model"]).mean(numeric_only=True).reset_index()
 
-        this_df = this_df.groupby("model").mean().reset_index()
         if model_order:
             this_df = this_df.loc[this_df["model"].isin(model_order)]
         this_metrics = METRIC_DICT[metric]["metric"]
@@ -115,7 +130,9 @@ def collect_outputs(path, norm, model_order=None):
         for i in range(len(this_metrics)):
             this_df2 = min_max(this_df, this_metrics[i], this_minmax[i], norm)
             this_df2 = pd.melt(
-                this_df2, id_vars=["model"], value_vars=[this_metrics[i]]
+                this_df2[["model", this_metrics[i]]],
+                id_vars=["model"],
+                value_vars=[this_metrics[i]],
             )
             df_list.append(this_df2)
 
@@ -139,7 +156,9 @@ def collect_outputs(path, norm, model_order=None):
     return df, df_non_agg
 
 
-def plot(save_folder, df, models, title, colors_list=None):
+def plot(save_folder, df, models, title, colors_list=None, norm="std"):
+
+    df = df.loc[df["model"].isin(models)]
     path = Path(save_folder)
     path.mkdir(parents=True, exist_ok=True)
 
@@ -172,13 +191,25 @@ def plot(save_folder, df, models, title, colors_list=None):
             this_model.append(val)
         all_models.append(this_model)
 
+    # import ipdb
+
+    # ipdb.set_trace()
+
     if len(models) == 5:
         colors = ["#636EFA", "#00CC96", "#AB63FA", "#FFA15A", "#EF553B"]
     elif len(models) == 4:
         colors = ["#636EFA", "#00CC96", "#AB63FA", "#EF553B"]
+    elif len(models) == 2:
+        colors = ["#636EFA", "#EF553B"]
     opacity = 1
     fill = "toself"
     fill = "none"
+
+    if norm == "std":
+        range_vals = [-2, 2]
+    else:
+        range_vals = [0, 1]
+
     fig = go.Figure(
         data=[
             go.Scatterpolar(
@@ -197,7 +228,7 @@ def plot(save_folder, df, models, title, colors_list=None):
         ],
         layout=go.Layout(
             title=go.layout.Title(text=f"{title}"),
-            polar={"radialaxis": {"visible": True, "range": [-2, 2], "dtick": 2}},
+            polar={"radialaxis": {"visible": True, "range": range_vals, "dtick": 2}},
             showlegend=True,
             margin=dict(l=170, r=150, t=120, b=80),
             legend=dict(orientation="h", xanchor="center", x=1.2, y=1.5),
@@ -211,3 +242,66 @@ def plot(save_folder, df, models, title, colors_list=None):
 
     fig.write_image(path / f"{title}.png")
     # fig.write_image(path / f"{title}.pdf")
+
+
+def plot_pc(directory, names, key, flip=False, alpha=None, views=['xy']):
+    """
+    Plot point clouds saved as csv's in a directory
+    directory - location where they are saved e.g. './test'
+    names - names of files e.g - ['G1', 'earlyS', ...]
+    key - type of filter used to generate the files e.g. - 'cell_cycle'
+    """
+    fnames = [i + '.csv' for i in names]
+
+    df = pd.DataFrame([])
+    for idx, _ in enumerate(fnames):
+        fname = fnames[idx]
+        print(fname)
+        dft = pd.read_csv(f"{directory}/{fname}", index_col=0)
+        dft[key] = names[idx]
+        df = pd.concat([df, dft], ignore_index=True)
+    
+    df, cmap = normalize_intensities_and_get_colormap(df=df, pcts=[5, 95])
+
+    for sub_key in df[key].unique():
+        df_sub = df.loc[df[key] == sub_key]
+        plt.style.use("default")
+        fig, axes = plt.subplots(1, len(views), figsize=(len(views)*2,2), dpi=150)
+        x = df_sub.x.values
+        y = df_sub.y.values
+        z = df_sub.z.values
+        # if flip:
+        #     y = df_sub.z.values
+        #     z = df_sub.y.values
+        orders = [[x, y], [x, z], [y, z]]
+        labels = ['xy', 'xz', 'yz']
+        if flip:
+            labels = ['xz', 'xy', 'yz']
+
+        for i in range(len(views)):
+            this_view = views[i]
+            ind = np.where(np.array(labels)==this_view)[0][0]
+            this_order = orders[ind]
+            this_label = labels[ind]
+            # valids = np.where((z>-0.5)&(z<0.5))
+            intensity = df_sub.inorm.values
+            # print(mu, bin, intensity.mean())
+            # x = x[valids]; y = y[valids]; intensity = intensity[valids]
+            # ax.set_facecolor("black")
+            try:
+                this_axes = axes[i]
+            except:
+                this_axes = axes
+            this_axes.scatter(this_order[0], this_order[1], c=cmap(intensity), s=3*intensity, alpha=alpha)
+            this_axes.set_title(f"{sub_key}_{this_label}")
+            this_axes.set_aspect("equal")
+            this_axes.axis("off")
+        plt.tight_layout()
+        # plt.show()
+
+        # fig.canvas.draw()
+        # image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+        # image = image.reshape(*reversed(fig.canvas.get_width_height()), 3)
+        fig.savefig(Path(directory) / Path(f'{sub_key}_clean.png'), bbox_inches='tight', dpi=300)
+        plt.close("all")
+        # seq.append(image)

src/features/reconstruction.py
@@ -43,9 +43,7 @@ def write_pyvista_latent_walk_gif(out_file, view, mesh_files, expl_var=None):
 
     mesh = pv.read(mesh_files[0])
     if mesh.points.shape[0] == 0:
-        vertices = np.array(
-            [[0, 0, 0], [1, 0, 0]]
-        )
+        vertices = np.array([[0, 0, 0], [1, 0, 0]])
         mesh = pv.PolyData(vertices)
     mesh.clear_data()
     plotter = pv.Plotter(window_size=[650, 600])
@@ -84,9 +82,7 @@ def write_pyvista_latent_walk_gif(out_file, view, mesh_files, expl_var=None):
     for i, f in enumerate(mesh_files_tmp):
         new_mesh = pv.read(f)
         if new_mesh.points.shape[0] == 0:
-            vertices = np.array(
-                [[0, 0, 0], [1, 0, 0]]
-            )
+            vertices = np.array([[0, 0, 0], [1, 0, 0]])
             new_mesh = pv.PolyData(vertices)
         new_mesh.clear_data()
         mesh.overwrite(new_mesh)
@@ -324,31 +320,49 @@ def get_mesh_from_sdf(sdf, method="skimage"):
     return mesh
 
 
-def save_pcloud(xhat, path, name):
+def save_pcloud(xhat, path, name, z_max, z_ind=2):
     """
     Save pointcloud xhat
+    z_max - percentage of z_max to place cut at
 
     """
     path = Path(path)
     path.mkdir(parents=True, exist_ok=True)
-    this_recon = pv.PolyData(xhat[:, :3])
-    this_recon.save(path / f"{name}.ply", texture=xhat[:, :].astype(np.uint8))
 
-    max_num = xhat[:, 2].max()
-    inds = np.where(xhat[:, 2] < 0.2 * max_num)[0]
-    xhat = xhat[inds]
-    inds = np.where(xhat[:, 2] > -0.2 * max_num)[0]
-    xhat = xhat[inds]
+    if torch.is_tensor(xhat):
+        xhat = xhat.detach().cpu().numpy()
+    # this_recon = pv.PolyData(xhat[:, :3])
+    # this_recon.save(path / f"{name}.ply", texture=xhat[:, :].astype(np.uint8))
+    if len(xhat[0,:]) == 4:
+        cols = ['x', 'y', 'z', 's']
+    else:
+        cols = ['x', 'y', 'z']
 
-    this_recon = pv.PolyData(xhat[:, :3])
-    this_recon.save(path / f"{name}_center.ply", texture=xhat[:, :].astype(np.uint8))
+    if z_max is None:
+        this_recon = pd.DataFrame(xhat, columns=cols)
+        this_recon.to_csv(path / f"{name}.csv")
+    else:
+        max_num = xhat[:, z_ind].max()
+        # ratio = 0.5
+        ratio = z_max
+        inds = np.where(xhat[:, z_ind] < ratio * max_num)[0]
+        xhat = xhat[inds]
+        inds = np.where(xhat[:, z_ind] > -ratio * max_num)[0]
+        xhat = xhat[inds]
+
+        # this_recon = pv.PolyData(xhat[:, :3])
+        # this_recon.save(path / f"{name}_center.ply", texture=xhat[:, :].astype(np.uint8))
+        this_recon = pd.DataFrame(xhat, columns=cols)
+        this_recon.to_csv(path / f"{name}.csv")
+    return xhat
 
 
 def make_canonical_shapes(
-    model, df, device, path, slice_key, sub_slice_list, max_embed_dim
+    model, df, device, path, slice_key, sub_slice_list, max_embed_dim, key, z_max=None, z_ind=2
 ):
     model = model.eval()
     cols = [i for i in df.columns if "mu" in i]
+    all_xhat = []
     for stage in sub_slice_list:
         this_stage_mu = (
             df.loc[df[slice_key] == stage][cols]
@@ -357,16 +371,21 @@ def make_canonical_shapes(
             .values
         )
         with torch.no_grad():
+            # z_inf = torch.tensor(this_stage_mu).mean(axis=0).unsqueeze(axis=0)
+            # idx = np.random.randint(this_stage_mu.shape[0], size=1)
+            # this_stage_mu = this_stage_mu[idx]
             z_inf = torch.tensor(this_stage_mu).mean(axis=0).unsqueeze(axis=0)
             z_inf = z_inf.to(device)
             z_inf = z_inf.float()
-            decoder = model.decoder
-            xhat = decoder(z_inf)
+            decoder = model.decoder[key]
+            xhat = decoder(z_inf).detach().cpu().numpy()
 
             if len(xhat.shape) > 3:
                 xhat = sample_points(xhat.detach().cpu().numpy())
 
-            save_pcloud(xhat[0], path, stage)
+            xhat = save_pcloud(xhat[0], path, stage, z_max, z_ind)
+            all_xhat.append(xhat)
+    return all_xhat
 
 
 def extract_digitized_shape_modes(shape_mode, shape_modes_df, pca, map_points):
@@ -411,6 +430,38 @@ def extract_digitized_shape_modes(shape_mode, shape_modes_df, pca, map_points):
     return df_inv
 
 
+def stratified_latent_walk(    
+    model,
+    device,
+    df,
+    x_label,
+    max_embed_dim,
+    latent_dim,
+    max_num_shapemodes,
+    path,
+    stratify_key,
+    z_max=None,
+    latent_walk_range=None,
+    z_ind=2,
+):
+    for strat in df[stratify_key].unique():
+        this_df = df.loc[df[stratify_key] == strat].reset_index(drop=True)
+        latent_walk(
+            model,
+            device,
+            this_df,
+            x_label=x_label,
+            max_embed_dim=max_embed_dim,
+            latent_dim=latent_dim,
+            max_num_shapemodes=max_num_shapemodes,
+            path=path,
+            z_max=z_max,
+            latent_walk_range=latent_walk_range,
+            z_ind=z_ind,
+            sub_key=strat,
+        )
+
+
 def latent_walk(
     model,
     device,
@@ -420,13 +471,21 @@ def latent_walk(
     latent_dim,
     max_num_shapemodes,
     path,
+    z_max=None,
     latent_walk_range=None,
+    z_ind=2,
+    sub_key=None,
 ):
     path = Path(path)
     path.mkdir(parents=True, exist_ok=True)
-
     cols = [i for i in df.columns if "mu" in i]
     all_features = df[cols].iloc[:, :max_embed_dim].dropna(axis=1).values
+
+    if all_features.shape[0] < 256:
+        n = 256 - all_features.shape[0]  # for 2 random indices
+        index = np.random.choice(all_features.shape[0], n, replace=False) 
+        random_sample = all_features[index] + 0.01*np.random.randn(256)
+        all_features = np.concatenate([all_features, random_sample], axis=0)
 
     pca = PCA(n_components=256)
     pca_features = pca.fit_transform(all_features)
@@ -460,8 +519,11 @@ def latent_walk(
                     xhat = sample_points(xhat.detach().cpu().numpy())
                 else:
                     xhat = xhat.detach().cpu().numpy()
-
-                save_pcloud(xhat[0], path, f"{rank}_{value_index}")
+                if sub_key is None:
+                    name = f"{rank}_{value_index}"
+                else:
+                    name = f"{sub_key}_{rank}_{value_index}"
+                save_pcloud(xhat[0], path, name, z_max, z_ind)
 
                 all_recons[f"{rank}"]["latent_point"].append(value)
                 all_recons[f"{rank}"]["recon"].append(xhat)

src/features/rotation_invariance.py
@@ -3,8 +3,14 @@ import numpy as np
 from tqdm import tqdm
 import pandas as pd
 import torch.nn.functional as F
+import warnings
 
-from pointcloudutils.networks import LatentLocalDecoder
+try:
+    from pointcloudutils.networks import LatentLocalDecoder
+except ImportError:
+    LatentLocalDecoder = None
+    warnings.warn("local_settings failed to import", ImportWarning)
+
 from scipy.spatial.transform import Rotation as R
 from src.models.predict_model import model_pass
 from src.models.utils import get_iae_reconstruction_3d_grid
@@ -177,10 +183,13 @@ def get_equiv_dict(
                         batch_input = {
                             this_key: torch.tensor(this_input_rot).to(device).float()
                         }
-                        if hasattr(this_model, 'decoder'):
-                            if isinstance(this_model.decoder[this_key], LatentLocalDecoder):
-                                batch_input["points"] = i["points"]
-                                batch_input["points.df"] = i["points.df"]
+                        if hasattr(this_model, "decoder"):
+                            if LatentLocalDecoder is not None:
+                                if isinstance(
+                                    this_model.decoder[this_key], LatentLocalDecoder
+                                ):
+                                    batch_input["points"] = i["points"]
+                                    batch_input["points.df"] = i["points.df"]
 
                         out, z, loss, _ = model_pass(
                             batch_input,

src/features/shapemodes.py
@@ -134,12 +134,11 @@ def digitize_shape_mode(
     # Check if feature is available
     if feature not in df.columns:
         raise ValueError(f"Column {feature} not found.")
-
     # Exclude extremeties
     df = filter_extremes_based_on_percentile(
         df=df, features=filter_based_on, pct=filter_extremes_pct
     )
-
+    df = df.reset_index(drop=True)
     # Get feature values
     values = df[feature].values.astype(np.float32)
 
@@ -158,8 +157,10 @@ def digitize_shape_mode(
     # Force samples below/above -/+ 2std to fall into first/last bin
     bin_centers = np.linspace(LINF, LSUP, nbins)
     bin_edges = np.unique([(b - binw, b + binw) for b in bin_centers])
+    bin_edges = [round(i, 2) for i in bin_edges]
     bin_edges[0] = -np.inf
     bin_edges[-1] = np.inf
+    bin_edges = np.unique(bin_edges)
 
     # Aplly digitization
     df["bin"] = np.digitize(values, bin_edges)
@@ -212,10 +213,12 @@ def get_shape_mode_bins(
             df,
             this_pc,
             n_bins,
-            pc_names,
+            # pc_names,
+            [this_pc],
             filter_extremes_pct,
             save,
         )
+        # print(df["bin"].value_counts())
         df.rename(columns={"bin": f"{this_pc}_bin"}, inplace=True)
 
     return df

src/features/stereotypy.py
@@ -11,6 +11,7 @@ from sklearn.metrics import mutual_info_score
 from scipy.stats import spearmanr
 from sklearn.decomposition import PCA
 from src.features.outlier_compactness import compute_MLE_intrinsic_dimensionality
+from scipy.spatial.distance import correlation
 
 
 def get_stereotypy_stratified(
@@ -26,16 +27,15 @@ def get_stereotypy_stratified(
     tmp_ret = []
     for model in all_ret["model"].unique():
         df2 = all_ret.loc[all_ret["model"] == model]
-
         for strat in df2[stratify_col].unique():
             df3 = df2.loc[df2[stratify_col] == strat]
             this_feats = df3[[i for i in df3.columns if "mu" in i]]
             this_feats = this_feats.dropna(axis=1).values
 
-            num, _ = compute_MLE_intrinsic_dimensionality(this_feats)
-            print(num, strat)
-            # pca = PCA(n_components=min(this_feats.shape[1], this_feats.shape[0]))
-            pca = PCA(n_components=int(num * 10))
+            _, num = compute_MLE_intrinsic_dimensionality(this_feats)
+            print(num)
+            pca = PCA(n_components=min(this_feats.shape[1], this_feats.shape[0]))
+            # pca = PCA(n_components=int(num))
             # pca = PCA(n_components=int(10))
             pca = pca.fit(this_feats)
             this_feats = pca.transform(this_feats)
@@ -130,9 +130,9 @@ def get_stereotypy(
         # this_feats = pca.transform(this_feats)
         # this_mo[[f"PCA_{i}" for i in range(this_feats.shape[1])]] = this_feats
 
-        for pc in range(8):
+        for pc in range(max_pcs):
             if pc < max_pcs:
-                for bin in range(9):
+                for bin in range(max_bins):
                     if bin < max_bins:
                         this_pc = pc + 1
                         this_bin = bin + 1
@@ -155,6 +155,8 @@ def get_stereotypy(
                                 this_ret = this_all_ret.loc[
                                     this_all_ret["structure_name"] == struct
                                 ].reset_index(drop=True)
+                                this_ret['pc'] = this_pc
+                                this_ret['bin'] = this_bin
 
                                 stereotypy, distances = correlate(
                                     this_ret, max_embed_dim
@@ -229,12 +231,26 @@ def get_stereotypy(
     return ret_dict_stereotypy, ret_dict_baseline_stereotypy, ret_corr
 
 
+from sklearn.feature_selection import mutual_info_regression
+
 def base_correlation_map(x, y):
     desired = np.empty((x.shape[0], y.shape[0]))
     for n in range(x.shape[0]):
         for m in range(y.shape[0]):
-            # desired[n, m] = pearsonr(x[n, :], y[m, :])[0]
-            desired[n, m] = spearmanr(x[n, :], y[m, :]).correlation
+            desired[n, m] = pearsonr(x[n, :], y[m, :])[0]
+            # desired[n, m] = spearmanr(x[n, :], y[m, :]).correlation
+
+            # desired[n, m] = dcor.independence.distance_correlation(x[n, :], y[m, :])
+            # significance = 0.1
+            # desired[n, m] = correlation(x[n, :], y[m, :])
+            # import ipdb
+            # ipdb.set_trace()
+            # desired[n, m] = mutual_info_regression(x[n, :].reshape(-1,1), y[m, :])[0]
+
+            # mine = MINE(alpha=0.6, c=15, est='mic_approx')
+            # mine.compute_score(x[n, :],y[m, :])
+            # desired[n, m] = round(mine.mic(),2)
+            #  = mutual_info_regression(x[n, :], y[m, :])
     return desired
 
 
@@ -270,8 +286,8 @@ def generate_correlation_map(x, y):
 
 def correlate(this_mo, max_embed_dim):
     """Correlation between representations for different cells"""
-    # cols = [i for i in this_mo.columns if "mu" in i]
-    cols = [i for i in this_mo.columns if "PCA" in i]
+    cols = [i for i in this_mo.columns if "mu" in i]
+    # cols = [i for i in this_mo.columns if "PCA" in i]
     this_feats = this_mo[cols].iloc[:, :max_embed_dim].dropna(axis=1).values
 
     distances = np.sum(
@@ -279,6 +295,20 @@ def correlate(this_mo, max_embed_dim):
     )
     stereotypy = generate_correlation_map(this_feats, this_feats)
     # stereotypy = base_correlation_map(this_feats, this_feats)
+
+    stereotypy *= np.tri(*stereotypy.shape)
+    np.fill_diagonal(stereotypy, 0)
+
+    distances *= np.tri(*distances.shape)
+    np.fill_diagonal(distances, 0)
+
+    distances[distances == 0] = np.nan
+    stereotypy[stereotypy == 0] = np.nan
+
+    stereotypy = np.abs(stereotypy)
+    distances = np.abs(distances)
+    print(stereotypy.shape, this_mo['rule'].unique(), this_mo['pc'].unique(), this_mo['bin'].unique(), np.nanmean(distances), np.nanmean(stereotypy))
+
     return stereotypy, distances
 
 
@@ -421,10 +451,7 @@ def make_variance_boxplots(base_path, path, pc_list, bin_list, save_folder):
     df_base["stereotypy"] = df_base["stereotypy"].abs()
     df_base = df_base[df_base["CellId_1"] != df_base["CellId_2"]]
 
-
-    df4 = pd.read_csv(
-        path
-    )
+    df4 = pd.read_csv(path)
     df4.drop_duplicates(subset=["stereotypy"])
     df4["stereotypy"] = df4["stereotypy"].abs()
     df4 = df4[df4["CellId_1"] != df4["CellId_2"]]