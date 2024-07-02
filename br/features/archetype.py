9a62795a5b5c7a224b15278cf01ae2e1491b43f5
Ritvik Vasan <ritvik.vasan@alleninstitute.org>
2024-07-02 12:42:51 -0700

    remove allencellmodeling refs

--- br/features/archetype.py
+++ br/features/archetype.py
@@ -2,9 +2,7 @@
 
 Algorithm is based on the paper: "Archetypal Analysis as an Autoencoder"
 (https://www.researchgate.net/publication/282733207_Archetypal_Analysis_as_an_Autoencoder)
-Code adapted from https://github.com/nichohelmut/football_results/blob/master/clustering/clustering.py
-
-Author: Guillermo García Cobo
+Code adapted from https://github.com/atmguille/archetypal-analysis/blob/main/Python%20implementation/AA_Fast.py
 """
 
 from abc import ABC, abstractmethod
@@ -129,7 +127,9 @@ class AA_Fast(AA_Abstract):
         super().__init__(n_archetypes, max_iter, tol, verbose)
         self.derivative_max_iter = derivative_max_iter
 
-    def _computeA(self, X: np.ndarray, Z: np.ndarray, A: np.ndarray = None) -> np.ndarray:
+    def _computeA(
+        self, X: np.ndarray, Z: np.ndarray, A: np.ndarray = None
+    ) -> np.ndarray:
         A = np.zeros((self.n_samples, self.n_archetypes))
         A[:, 0] = 1.0
         e = np.zeros(A.shape)
@@ -143,7 +143,9 @@ class AA_Fast(AA_Abstract):
             e[range(self.n_samples), argmins] = 0.0
         return A
 
-    def _computeB(self, X: np.ndarray, A: np.ndarray, B: np.ndarray = None) -> np.ndarray:
+    def _computeB(
+        self, X: np.ndarray, A: np.ndarray, B: np.ndarray = None
+    ) -> np.ndarray:
         B = np.zeros((self.n_archetypes, self.n_samples))
         B[:, 0] = 1.0
         e = np.zeros(B.shape)
