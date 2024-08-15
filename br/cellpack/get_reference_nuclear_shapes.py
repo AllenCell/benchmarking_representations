bcc05c38325ee14d1b052555de8ac6904bc65d07
Saurabh Mogre <saurabh.mogre@alleninstitute.org>
2024-08-15 10:14:25 -0700

    Update code to fetch reference nuclear shapes from S3

--- br/cellpack/get_reference_nuclear_shapes.py
+++ br/cellpack/get_reference_nuclear_shapes.py
@@ -1,9 +1,23 @@
 # %%
 import quilt3
+import os
 
 # %%
-pkg = quilt3.Package.browse(
-    "aics/morphology_appropriate_representation_learning", registry="s3://allencell"
+b = quilt3.Bucket("s3://allencell")
+
+b.ls(
+    "aics/morphology_appropriate_representation_learning/"
+    "cellPACK_single_cell_punctate_structure/reference_nuclear_shapes/"
 )
 
 # %%
+datadir = os.path.join(
+    os.path.dirname(__file__), "..", "data", "cellpack", "reference_nuclear_shapes"
+)
+os.makedirs(datadir, exist_ok=True)
+# %%
+b.fetch(
+    "aics/morphology_appropriate_representation_learning/"
+    "cellPACK_single_cell_punctate_structure/reference_nuclear_shapes/",
+    f"{datadir}/",
+)
