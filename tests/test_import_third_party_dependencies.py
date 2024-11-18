33a50307155c6bc2ba8d5f969211501aa083167c
Philip Garrison <philip.garrison@alleninstitute.org>
2024-11-18 10:58:45 -0800

    Add test for lie_learn installation

---
+++ tests/test_import_third_party_dependencies.py
@@ -0,0 +1,5 @@
+# These tests are for validating the installation of specific dependencies that
+# our instructions have not always installed correctly.
+
+def test_import_lie_learn():
+    import lie_learn.representations.SO3.irrep_bases
