32816ac8612da5aeb27037d656f55b9a55d714bb
Ritvik Vasan <ritvik.vasan@prd-aics-dcp-039.corp.alleninstitute.org>
2024-11-27 11:14:41 -0800

    add snakefile and separate config group for pcna dataset

--- subpackages/image_preprocessing/image_preprocessing/steps/combine_dfs.py
+++ subpackages/image_preprocessing/image_preprocessing/steps/combine_dfs.py
@@ -11,7 +11,8 @@ class CombineDataframes(Step):
     def run(self, df, n_workers=None):
         more_columns_to_drop = [col for col in df.columns if "_dropme" in col]
         df = df.drop(columns=more_columns_to_drop + self.columns_to_drop)
-        df["meta_colony_centroid"] = df["meta_colony_centroid"].fillna("(-1, -1)")
+        if "meta_colony_centroid" in df.columns:
+            df["meta_colony_centroid"] = df["meta_colony_centroid"].fillna("(-1, -1)")
 
         for col in df.dtypes[df.dtypes == np.dtype("O")].index.tolist():
             df[col] = df[col].astype(str)
