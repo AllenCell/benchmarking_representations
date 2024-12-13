530f16e2f2bb7760673c58e198f7646577f577c9
Ritvik Vasan <ritvik.vasan@prd-aics-dcp-036.corp.alleninstitute.org>
2024-12-13 10:33:01 -0800

    add SDF processing scripts

---
+++ src/br/data/preprocessing/sdf_preprocessing/get_max_bounding_box.py
@@ -0,0 +1,94 @@
+
+import pandas as pd
+from aicsimageio import AICSImage
+from monai.transforms import FillHoles
+from tqdm import tqdm
+from br.data.utils import (
+    get_mesh_from_image,
+)
+from multiprocessing import Pool
+from pathlib import Path
+import argparse
+
+
+def get_bounds(r):
+    return_df = {'x_delta': [], 'y_delta': [], 'z_delta': [], 'cell_id': [], 'max_delta': []}
+    cellid = r['CellId']
+
+    hole_fill_transform = FillHoles()
+    seg = AICSImage(r["crop_seg_masked"]).data.squeeze()
+    seg = hole_fill_transform(seg).numpy()
+
+    mesh, _, _ = get_mesh_from_image(seg, sigma=0, lcc=False, denoise=False)
+
+    bounds = mesh.GetBounds()
+    x_delta = bounds[1] - bounds[0]
+    y_delta = bounds[3] - bounds[2]
+    z_delta = bounds[5] - bounds[4]
+    max_delta = max([x_delta, y_delta, z_delta])
+    return_df['x_delta'].append(x_delta)
+    return_df['y_delta'].append(y_delta)
+    return_df['z_delta'].append(z_delta)
+    return_df['cell_id'].append(cellid)
+    return_df['max_delta'].append(max_delta)
+
+    return return_df
+
+def main(args):
+    # make save path directory
+    Path(args.save_path).mkdir(parents=True, exist_ok=True)
+
+    df = pd.read_csv(args.manifest)
+
+    if args.global_path:
+        df["crop_seg_masked"] = df["crop_seg_masked"].apply(lambda x: args.global_path + x)
+
+    all_rows = []
+    for _, row in df.iterrows():
+        all_rows.append(row)
+
+    with Pool(1) as p:
+        jobs = tuple(
+            tqdm(
+                p.imap_unordered(
+                    get_bounds,
+                    all_rows,
+                ),
+                total=len(all_rows),
+                desc="get_bounds",
+            )
+        )
+        jobs = [i for i in jobs if i is not None]
+        return_df = pd.DataFrame(jobs).reset_index(drop=True)
+
+    return_df.to_csv(Path(args.save_path) / Path('bounds.csv'))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Script for getting global mesh bounds for polymorphic structures from WTC-11 hIPS single cell image dataset"
+    )
+    parser.add_argument("--save_path", type=str, required=True, help="Path to save results.")
+    parser.add_argument(
+        "--manifest",
+        type=str,
+        required=True,
+        help="Path to preprocessed single cell image manifest.",
+    )
+    parser.add_argument(
+        "--global_path",
+        type=str,
+        default=None,
+        required=False,
+        help="Path to append to relative paths in preprocessed manifest",
+    )
+
+
+    args = parser.parse_args()
+    main(args)
+
+    """
+    Example run:
+    python pc_sdfs.py --save_path "./test_pcs/" --scaled_mesh_path "./test_img/outputs_mesh/" --vox_resolution 32
+    """
+
--- src/br/data/preprocessing/sdf_preprocessing/image_sdfs.py
+++ src/br/data/preprocessing/sdf_preprocessing/image_sdfs.py
@@ -1,95 +1,136 @@
+import argparse
+from multiprocessing import Pool
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 import pyvista as pv
 from aicsimageio import AICSImage
 from monai.transforms import FillHoles
+from tqdm import tqdm
+
+from br.analysis.analysis_utils import str2bool
 from br.data.utils import (
     get_mesh_from_image,
     get_scaled_mesh,
     get_sdf_from_mesh_vtk,
-    voxelize_scaled_mesh
+    voxelize_scaled_mesh,
 )
-from tqdm import tqdm
-from multiprocessing import Pool
 
-path = './morphology_appropriate_representation_learning/preprocessed_data/npm1/manifest.csv'
-scale_factor_path = './morphology_appropriate_representation_learning/preprocessed_data/npm1/scale_factor.npz'
-out_dir_scaled_seg = "./outputs_seg_npm1/"
-out_dir_scaled_sdf = "./outputs_sdf_npm1/"
-out_dir_mesh = "./outputs_mesh_npm1/"
-
-sc_factor_data = np.load(scale_factor_path, allow_pickle=True)
-scale_factor_dict = dict(zip(sc_factor_data["keys"], sc_factor_data["values"]))
-
-df = pd.read_csv(path)
-
-
-test_ids = [964798, 661110, 644401, 967887, 703621, 644479]
-df = df.loc[df['CellId'].isin(test_ids)].reset_index(drop=True)
-hole_fill_transform = FillHoles()
-
-all_rows = []
-for ind, row in tqdm(df.iterrows(), total=len(df)):
-    all_rows.append(row)
 
 def process(r):
-    try:
-        cellid = r["CellId"]
+    cellid = r["CellId"]
+    scale_factor = r["scale_factor"]
+    vox_resolution = r["vox_resolution"]
 
-        out_path_sdf = f"{out_dir_scaled_sdf}/{cellid}"
-        out_path_seg = f"{out_dir_scaled_seg}/{cellid}"
-        out_path_mesh = f"{out_dir_mesh}/{cellid}.stl"
+    out_path_sdf = f"{out_dir_scaled_sdf}/{cellid}"
+    out_path_seg = f"{out_dir_scaled_seg}/{cellid}"
+    out_path_mesh = f"{out_dir_mesh}/{cellid}.stl"
+    hole_fill_transform = FillHoles()
+    seg = AICSImage(r["crop_seg_masked"]).data.squeeze()
+    seg = hole_fill_transform(seg).numpy()
 
-        seg = AICSImage(r["crop_seg_masked"]).data.squeeze()
-        seg = hole_fill_transform(seg).numpy()
+    mesh, _, _ = get_mesh_from_image(seg, sigma=0, lcc=False, denoise=False)
 
-        # print(seg.shape)
+    pv.wrap(mesh).save(out_path_mesh)
 
-        mesh, _, _ = get_mesh_from_image(seg, sigma=0, lcc=False, denoise=False)
+    sdf, scale_factor = get_sdf_from_mesh_vtk(
+        None, vox_resolution=vox_resolution, scale_factor=scale_factor, vpolydata=mesh
+    )
+    np.save(out_path_sdf, sdf)
+    vox_shape = (vox_resolution, vox_resolution, vox_resolution)
+    scaled_mesh, _ = get_scaled_mesh(None, int(vox_resolution), scale_factor, mesh, True)
 
-        pv.wrap(mesh).save(out_path_mesh)
-        vox_resolution = 64
-        # vox_resolution = 32
+    scaled_seg = voxelize_scaled_mesh(scaled_mesh)
+    com = scaled_seg.shape
+    pad = []
+    for i, j in zip(vox_shape, com):
+        pad.append((int(i - j) // 2, int(i - j) // 2))
+    scaled_seg = np.pad(scaled_seg, pad)
 
-        sdf, scale_factor = get_sdf_from_mesh_vtk(
-            None, vox_resolution=vox_resolution, scale_factor=global_scale_factor_64_res, vpolydata=mesh
+    np.save(out_path_seg, scaled_seg)
+
+
+def main(args):
+
+    # make save path directory
+    Path(args.save_path).mkdir(parents=True, exist_ok=True)
+
+    global out_dir_scaled_seg
+    global out_dir_scaled_sdf
+    global out_dir_mesh
+
+    out_dir_scaled_seg = Path(args.save_path) / Path("outputs_seg")
+    out_dir_scaled_sdf = Path(args.save_path) / Path("outputs_sdf")
+    out_dir_mesh = Path(args.save_path) / Path("outputs_mesh")
+
+    Path(out_dir_scaled_seg).mkdir(parents=True, exist_ok=True)
+    Path(out_dir_scaled_sdf).mkdir(parents=True, exist_ok=True)
+    Path(out_dir_mesh).mkdir(parents=True, exist_ok=True)
+
+    scale_factor_path = Path(args.manifest).parent / Path("scale_factor.npz")
+
+    sc_factor_data = np.load(scale_factor_path, allow_pickle=True)
+    scale_factor_dict = dict(zip(sc_factor_data["keys"], sc_factor_data["values"]))
+
+    df = pd.read_csv(args.manifest)
+
+    if args.global_path:
+        df["crop_seg_masked"] = df["crop_seg_masked"].apply(lambda x: args.global_path + x)
+
+    if args.debug:
+        df = df.sample(n=5).reset_index(drop=True)
+
+    all_rows = []
+    for _, row in tqdm(df.iterrows(), total=len(df)):
+        row["scale_factor"] = scale_factor_dict[row["CellId"]]
+        row["vox_resolution"] = args.vox_resolution
+        all_rows.append(row)
+
+    with Pool(40) as p:
+        _ = tuple(
+            tqdm(
+                p.imap_unordered(
+                    process,
+                    all_rows,
+                ),
+                total=len(all_rows),
+                desc="compute_everything",
+            )
         )
-        np.save(out_path_sdf, sdf)
-        vox_shape = (vox_resolution, vox_resolution, vox_resolution)
-        scaled_mesh, _ = get_scaled_mesh(
-            None, int(vox_resolution), global_scale_factor_64_res, mesh, True
-        )
 
-        # coords = numpy_support.vtk_to_numpy(scaled_mesh.GetPoints().GetData())
-        # centroid = coords.mean(axis=0, keepdims=True)
-        # print(centroid)
 
-        scaled_seg = voxelize_scaled_mesh(scaled_mesh)
-        com = scaled_seg.shape
-        print(com)
-        pad = []
-        for i, j in zip(vox_shape, com):
-            pad.append((int(i - j)//2, int(i - j)//2))
-        scaled_seg = np.pad(scaled_seg, pad)
-        # scaled_seg = get_image_from_mesh(scaled_mesh, vox_shape, 0)
-
-        np.save(out_path_seg, scaled_seg)
-    except:
-        print(r['CellId'])
-
-# with Pool(40) as p:
-with Pool(1) as p:
-    _ = tuple(
-        tqdm(
-            p.imap_unordered(
-                process,
-                all_rows,
-            ),
-            total=len(all_rows),
-            desc="compute_everything",
-        )
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Script for computing scaled segmentations and SDFs for polymorphic structures from WTC-11 hIPS single cell image dataset"
+    )
+    parser.add_argument("--save_path", type=str, required=True, help="Path to save results.")
+    parser.add_argument(
+        "--manifest",
+        type=str,
+        required=True,
+        help="Path to preprocessed single cell image manifest.",
+    )
+    parser.add_argument(
+        "--vox_resolution",
+        type=int,
+        required=True,
+        help="Resolution to voxelize images to",
+    )
+    parser.add_argument("--debug", type=str2bool, default=False, help="Enable debug mode.")
+    parser.add_argument(
+        "--global_path",
+        type=str,
+        default=None,
+        required=False,
+        help="Path to append to relative paths in preprocessed manifest",
     )
 
+    args = parser.parse_args()
+    main(args)
 
-# for i, r in tqdm(df.iterrows(), total=len(df)):
+    """
+    Example run:
 
+    python image_sdfs.py --save_path "./test_img/" --manifest "/allen/aics/modeling/ritvik/projects/latest_clones/benchmarking_representations/morphology_appropriate_representation_learning/preprocessed_data/npm1/manifest.csv" --vox_resolution 32 --debug True --global_path "../../../../../"
+    """
